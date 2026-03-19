17d80a9ea597f0e64292849e9226c7d5330d9577
joaobaratella <jpbaratella07@gmail.com>
2026-03-19 14:19:33 -0300

    Primeira versao da funcao produtoria

--- main.c
+++ main.c
@@ -27,8 +27,24 @@ long int somatoriaRecursiva(int x,int y){
 }
 
 
+//produtorioRecursivo
+long int produtoriaRecursiva(int x, int y){
+ if(y>=x){
+   //long int produto=y;
+   long int resultado = produtoriaRecursiva(x,y-1);
+   printf("%ld - ", resultado);
+   return y*resultado;  
+ }
+ else{
+   return 1;
+ }
+}
+
+
 int main(void) {
-printf("\n\n :::::::::  INICIO DO PROGRAMA ::::::: \n\n");
+
+
+   printf("\n\n :::::::::  INICIO DO PROGRAMA ::::::: \n\n");
    printf("\n\nDigite o número x: ");
    scanf("%d",&x);
    printf("\n\nDigite o número y: ");
@@ -36,7 +52,7 @@ printf("\n\n :::::::::  INICIO DO PROGRAMA ::::::: \n\n");
 
 
    printf("\nA somatória é: %ld",somatoriaRecursiva(x,y));
-
-
+   printf("\nA produtória é: %ld",produtoriaRecursiva(x,y));
+  
  return 0;
 }
